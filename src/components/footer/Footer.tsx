d25f3928419dc58b4beae415d622f22d0c3b76c2
nsanzimfura-eric <erickykress1@gmail.com>
2024-04-22 11:39:58 +0200

    Style navbar and footer -Ensure that they are all responsiveness across all media devices

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,19 +1,20 @@
-import styles from './footer.module.scss'
+import styles from './footer.module.scss';
+import { Container } from 'react-bootstrap';
 
 const Footer = () => {
 
     const year = new Date().getFullYear();
     return (
         <footer className={`${styles.footer} w-100 m-0`}>
-            <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-                <div className='d-flex w-100'>
+            <Container className="w-100 py-5 ">
+                <div className='d-flex w-100 justify-content-between align-items-center'>
                     <img src="/icons/heart.svg" alt="Icon" />
                     <img src="/icons/important.svg" alt="Icon" />
                     <img src="/icons/bell.svg" alt="Icon" />
                 </div>
-                <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-                <span className="block text-sm text-white sm:text-center">© {year} <a href="https://nsanzimfura.web.app/" className="hover:underline">Ride Share Tracker™</a>. All Rights Reserved.</span>
-            </div>
+                <hr className="my-3 line d-none d-sm-block" />
+                <span className="d-none d-sm-flex justify-content-center align-items-center copyright">© {year} <a href="https://nsanzimfura.web.app/" >Ride Share Tracker.™</a> All Rights Reserved.</span>
+            </Container>
         </footer>
     );
 };
